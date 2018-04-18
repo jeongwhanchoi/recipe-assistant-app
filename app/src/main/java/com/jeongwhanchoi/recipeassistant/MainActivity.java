2b881d528f0ba2a55fa515cd1a3dd4ef36c02e50
jeongwhanchoi <jeongwhan.choi@icloud.com>
2018-04-19 02:13:19 +0900

    Delete comments

--- app/src/main/java/com/jeongwhanchoi/recipeassistant/MainActivity.java
+++ app/src/main/java/com/jeongwhanchoi/recipeassistant/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-//import com.google.firebase.messaging.FirebaseMessaging;
 import com.jeongwhanchoi.recipeassistant.fragment.AddRecipeFragment;
 import com.jeongwhanchoi.recipeassistant.fragment.CategoriesFragment;
 import com.jeongwhanchoi.recipeassistant.fragment.CategoryRecipesFragment;
